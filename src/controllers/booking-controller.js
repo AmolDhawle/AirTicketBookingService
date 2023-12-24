e76da958300525cb83d012ae6e0fd60e8b12b593
Amol Dhawle <amoldhawle2@gmail.com>
2023-12-24 21:56:42 +0530

    Added rabbit mq config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,25 +3,41 @@ const { BookingService } = require('../services/index');
 
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            message: "Successfully completed booking",
-            success: true,
-            err: {},
-            data: response
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message,
-            success: false,
-            err: error.explanation,
-            data: {}
-        })
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
+
+class BookingController{
+
+    constructor() {
     }
+
+    async sendMessageToQueue (req, res) {
+        const channel = await createChannel();
+        const data = { message: 'Success'};
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message: 'Successfully published the event' 
+        });
+    }
+
+    async create (req, res) {
+        try {
+            const response = await bookingService.createBooking(req.body);
+            return res.status(StatusCodes.OK).json({
+                message: "Successfully completed booking",
+                success: true,
+                err: {},
+                data: response
+            });
+        } catch (error) {
+            return res.status(500).json({
+                message: error.message,
+                success: false,
+                err: error.explanation,
+                data: {}
+            })
+        }
+    }   
 }
 
-module.exports = {
-    create
-}
+module.exports = BookingController;
