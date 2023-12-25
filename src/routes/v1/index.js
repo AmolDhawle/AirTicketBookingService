93d69a4a2def759b0743bc71256282aa199a7f87
Amol Dhawle <amoldhawle2@gmail.com>
2023-12-25 17:03:25 +0530

    added MQ channel to the controller

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
 const {BookingController} = require("../../controllers/index");
-// const { createChannel } = require("../../utils/messageQueue");
 
 const router = express.Router();
 
-// const channel = await createChannel();
 const bookingController = new BookingController();
 
 router.post("/bookings", bookingController.create);
